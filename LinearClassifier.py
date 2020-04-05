c710dae6f6a906ca550c60e81e676927e69757e5
Raphael Adamski <raphaeladamski@hotmail.com>
2020-04-05 00:02:59 -0300

    Create estimator to make training and predictions

---
+++ LinearClassifier.py
@@ -0,0 +1,31 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.multiclass import unique_labels
+import numpy as np
+
+
+class LinearClassifier(BaseEstimator, ClassifierMixin):
+    weights_ = None
+
+    def __init__(self, gamma=1):
+        self.gamma = gamma
+
+    def fit(self, X, y):
+        self.classes_ = unique_labels(y)
+        y_encoded = np.zeros((len(y), len(self.classes_)))
+        np.put_along_axis(y_encoded, y.reshape(-1, 1), 1, axis=1)
+        self.weights_ = np.linalg.inv(X.transpose().dot(X) + self.gamma * np.identity(X.shape[1])).dot(X.transpose().dot(y_encoded))
+        return self
+
+    def predict(self, X):
+        y = X.dot(self.weights_)
+        #  need to convert hot encoded to labels
+        return y.argmax(axis=1)
+
+    def get_params(self, deep=True):
+        # suppose this estimator has parameters "alpha" and "recursive"
+        return {"gamma": self.gamma}
+
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+        return self
--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python3
-from numpy import ndarray
-import numpy as np
 import scipy.io
 from sklearn.metrics import classification_report
-from sklearn.preprocessing import OneHotEncoder
-
-
-def calculate_weights(x: ndarray, y: ndarray, gamma: float = 1.0) -> ndarray:
-    #  inverse(x_t*x + gamma*I)*x_t*y
-    return np.linalg.inv(x.transpose().dot(x) + gamma * np.identity(x.shape[1])).dot(x.transpose().dot(y))
-
-
-def predict(x: ndarray, w: ndarray) -> ndarray:
-    return x.dot(w)
-
+from  LinearClassifier import LinearClassifier
 
 if __name__ == '__main__':
     # loas MATLAB matrix
@@ -28,13 +16,8 @@ if __name__ == '__main__':
     Xt = test['Xt']
     Yt = test['St']
 
-    W = calculate_weights(X, Y)
-    y_pred = predict(Xt, W)
-
-    #  need to convert regression to labels
-    y_pred = y_pred.argmax(axis=1)
-    #  one hot encoding
-    onehot_encoder = OneHotEncoder(categories=[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]], sparse=False, dtype="uint8")
-    y_pred_encoded = onehot_encoder.fit_transform(y_pred.reshape(-1, 1))
+    lin = LinearClassifier()
+    lin.fit(X, Y.argmax(axis=1))
+    y_pred = lin.predict(Xt)
 
     print(classification_report(Yt.argmax(axis=1), y_pred, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
